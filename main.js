0fd9e96b3cbd6e002896642996a7fb8c9c71b610
d1srupt0r <disruptorsgames@gmail.com>
2017-02-10 19:40:42 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow, webFrame} = require('electron')
+const path = require('path')
+const url = require('url')
+//const checker = require('spellchecker')
+
+let mainWindow
+
+webFrame.setSpellCheckProvider('en-US', true, {
+  spellCheck (text) {
+    return !(require('spellchecker').isMisspelled(text))
+  }
+})
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
