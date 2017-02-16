24b95f16b57a30ce91d6f49899d6f3d602132bce
d1srupt0r <disruptorsgames@gmail.com>
2017-02-16 01:03:38 -0800

    remove spellchecker (will try later); remove menu from main app; switch name; update package to require electron; added launch and tasks file for vscode

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-const {app, BrowserWindow, webFrame} = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-//const checker = require('spellchecker')
 
 let mainWindow
-
-webFrame.setSpellCheckProvider('en-US', true, {
-  spellCheck (text) {
-    return !(require('spellchecker').isMisspelled(text))
-  }
-})
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
@@ -19,6 +12,7 @@ function createWindow () {
     slashes: true
   }))
   mainWindow.webContents.openDevTools()
+  mainWindow.setMenu(null)
   mainWindow.on('closed', function () {
     mainWindow = null
   })
