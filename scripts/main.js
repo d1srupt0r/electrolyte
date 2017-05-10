311883ec3a919908575c074ee7d137709bac5264
d1srupt0r <disruptorsgames@gmail.com>
2017-05-09 22:54:31 -0700

    refactor file structure based on examples of requirejs

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '../index.html'),
     protocol: 'file:',
     slashes: true
   }))
